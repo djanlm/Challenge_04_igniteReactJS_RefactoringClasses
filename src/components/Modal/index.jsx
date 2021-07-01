189025053e4466b4aa2b35126cd3a11cbd7fdc61
Djanilson Martins <djanlm@hotmail.com>
2021-07-01 08:22:34 -0300

    all functions

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,58 +1,98 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+// import { Component } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+function Modal(props) {
+  const { isOpen, setIsOpen, children } = props;
+  const [modalStatus, setModalStatus] = useState(isOpen);
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
 
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
+// class Modal extends Component {
+//   constructor(props) {
+//     super(props);
 
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+//     const { isOpen } = this.props;
+//     this.state = {
+//       modalStatus: isOpen
+//     }
+//   }
 
-    return (
-      <ReactModal
-        shouldCloseOnOverlayClick={!false}
-        onRequestClose={setIsOpen}
-        isOpen={modalStatus}
-        ariaHideApp={false}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: '#F0F0F5',
-            color: '#000000',
-            borderRadius: '8px',
-            width: '736px',
-            border: 'none',
-          },
-          overlay: {
-            backgroundColor: '#121214e6',
-          },
-        }}
-      >
-        {children}
-      </ReactModal>
-    );
-  }
-};
+//   componentDidUpdate(prevProps) {
+//     const { isOpen } = this.props;
+
+//     if (prevProps.isOpen !== isOpen) {
+//       console.log(this.props)
+//       this.setState({ modalStatus: isOpen })
+//     }
+//   }
+
+//   render() {
+//     const { children, setIsOpen } = this.props;
+//     const { modalStatus } = this.state;
+
+//     return (
+//       <ReactModal
+//         shouldCloseOnOverlayClick={!false}
+//         onRequestClose={setIsOpen}
+//         isOpen={modalStatus}
+//         ariaHideApp={false}
+//         style={{
+//           content: {
+//             top: '50%',
+//             left: '50%',
+//             right: 'auto',
+//             bottom: 'auto',
+//             marginRight: '-50%',
+//             transform: 'translate(-50%, -50%)',
+//             background: '#F0F0F5',
+//             color: '#000000',
+//             borderRadius: '8px',
+//             width: '736px',
+//             border: 'none',
+//           },
+//           overlay: {
+//             backgroundColor: '#121214e6',
+//           },
+//         }}
+//       >
+//         {children}
+//       </ReactModal>
+//     );
+//   }
+// };
 
 export default Modal;
